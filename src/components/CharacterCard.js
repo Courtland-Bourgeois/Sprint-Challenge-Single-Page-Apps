92491d75ca21913acf0f1fd4302133681316fa01
Courtland Bourgeois <courtland@CourtlandsMBP2.eatel.net>
2019-11-15 12:23:51 -0600

    completed mvp

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,11 +1,17 @@
 import React from "react";
+import styled from 'styled-components';
+
+const CharCard = styled.div`
+  text-align: center;
+`;
+
 
 export default function CharacterCard(props) {
   return (
-    <div>
+    <CharCard>
       <h1>{props.name}</h1>
       <p>Species: {props.species}</p>
       <p>Status: {props.status}</p>
-    </div>
+    </CharCard>
   );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -5,6 +5,7 @@ import CharacterCard from './CharacterCard';
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
   const [list, setList] = useState([]);
+  const [query, setQuery] = useState("");
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
@@ -12,24 +13,44 @@ export default function CharacterList() {
     axios
       .get('https://rickandmortyapi.com/api/character/')
       .then(res => {
-        console.log(res)
-        setList(res.data.results)
+        const filter = res.data.results.filter(name => 
+          name.name.toLowerCase().includes(query.toLowerCase()))
+        // console.log(res)
+        setList(filter)
       })
       .catch(err => {
         console.log(err, "Oops")
       })
-  }, []);
+  }, [query]);
+
+  const handleInputChange = event => {
+    setQuery(event.target.value);
+  }
 
   return (
     <section className="character-list">
-      {list.map(char => (
-         <CharacterCard 
-          key={char.id} 
-          name={char.name}
-          species={char.species}
-          status={char.status}
-          /> 
-      ))}
+      <div>
+        <form>
+          <input
+            type="text"
+            onChange={handleInputChange}
+            value={query}
+            name="name"
+            placeholder="Search by Name"
+          />
+        </form>
+      </div>
+      <div>
+        {list.map(char => (
+          <CharacterCard 
+            key={char.id} 
+            name={char.name}
+            species={char.species}
+            status={char.status}
+            image={char.image}
+            /> 
+        ))}
+      </div>
     </section>
   );
 }
